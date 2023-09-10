2941bb6255a18ac7e4f1d54fd7df673a951f7337
tanjung anggraini agustin <108222692+4321224@users.noreply.github.com>
2023-09-10 23:38:37 +0700

    initial commit

---
+++ src/test/kotlin/com/tanjung/NoteApp/NoteAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tanjung.NoteApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoteAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
